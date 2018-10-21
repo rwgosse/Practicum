c10152be2549167bebc1bff7fad29088d64e2899
RichardGosse <rwgosse@gmail.com>
2018-10-21 16:38:42 -0700

    #networking

--- main.py
+++ main.py
@@ -356,20 +356,20 @@ def send_msg(sock, msg):
     msg = struct.pack('>I', len(msg)) + msg
     sock.sendall(msg)
 
-def recv_msg(sock):
+def recv_msg(rsock):
     # Read message length and unpack it into an integer
-    raw_msglen = recvall(sock, 4)
+    raw_msglen = recvall(rsock, 4)
     if not raw_msglen:
         return None
     msglen = struct.unpack('>I', raw_msglen)[0]
     # Read the message data
-    return recvall(sock, msglen)
+    return recvall(rsock, msglen)
 
-def recvall(sock, n):
+def recvall(rcsock, n):
     # Helper function to recv n bytes or return None if EOF is hit
     data = b''
     while len(data) < n:
-        packet = sock.recv(n - len(data))
+        packet = rcsock.recv(n - len(data))
         if not packet:
             return None
         data += packet # knows this is a byte object WTF!!!
@@ -422,7 +422,7 @@ def findchains():
                 
                 
                 
-                data_variable = pickle.loads(total_data)
+                data_variable = pickle.loads(total_data) # a bytes types object is required. yet total_data keeps appearing as NoneType :(
                 
                 chain = data_variable
                 foreign_chains.append(chain)
