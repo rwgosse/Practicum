bf81720be3a60d941abe0b5381bfc96440979caa
RichardGosse <rwgosse@gmail.com>
2018-11-13 20:46:42 -0800

    #oops broke chain

--- main.py
+++ main.py
@@ -154,7 +154,7 @@ class ChainServer(object):
             chain_client_socket, chain_client_address = self.sock.accept() # accept incomming connection
             threading.Thread(target=self.serve_chain, args=(chain_client_socket, chain_client_address)).start() # pass connection to a new thread
 
-    def serve_chain(self, client_client_socket, chain_client_address): # incomming connection request
+    def serve_chain(self, chain_client_socket, chain_client_address): # incomming connection request
         try: # get our local chain of blocks
             if os.path.exists(BLOCKCHAIN_DATA_DIR): # assuming the folder exists...
                 for filename in os.listdir(BLOCKCHAIN_DATA_DIR): # for every file...
